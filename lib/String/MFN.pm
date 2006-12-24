07d2693ec4507e818cef18ec986b9fadbdd423fb
Shawn Boyette <mdxi@cpan.org>
2006-12-24 08:24:03 +0000

    Import of MDXI/String-MFN-1.28 from CPAN.
    gitpan-cpan-distribution: String-MFN
    gitpan-cpan-version:      1.28
    gitpan-cpan-path:         MDXI/String-MFN-1.28.tar.gz
    gitpan-cpan-author:       MDXI
    gitpan-cpan-maturity:     released

--- lib/String/MFN.pm
+++ lib/String/MFN.pm
@@ -1,119 +1,118 @@
 package String::MFN;
 
-require Exporter;
+require 5.008;
 use warnings;
 use strict;
 
-our @ISA       = qw(Exporter);
-our @EXPORT    = qw(&mfn);
+use Encode;
+
+require Exporter;
+our @ISA       = qw( Exporter );
+our @EXPORT    = qw( &mfn );
 
 =head1 NAME
 
-String::MFN - 'Normalize' a string in the manner of the mfn utility
+String::MFN - Normalize a string to produce a sane Unix filename
 
 =head1 VERSION
 
-Version 1.27
+Version 1.28
 
 =cut
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 =head1 SYNOPSIS
 
     use String::MFN;
 
-    my $sane_string = mfn($retarded_string);
-    ...
+    my $clean = mfn($dirty);
 
 =head1 DESCRIPTION
 
-Normalizes a string. Normalization, in brief, means modifying the
-string to resemble a sane UNIX[TM] filename while retaining
-information carried by the original formatting.
+String::MFN exports a single function, C<mfn()>, which modifies a
+string to resemble a sane Unix filename.
 
-Normalization, in specific, consists of characters other than
-C<[\w\-\.\+]> being removed, lowercasing of all letters,
-separation of internaCaps, separation of leading numerals from
-trailing non-numerals, replacement of "bracketing" characters (C<<<
-{[(<>)]} >>>), replacement of ampersands, and collapsing (things that
-look like) repeating extentions.
-
-Some concrete examples (filenames found by googling for "mp3 playlist"):
-
-    Frank Sinatra & Count Basie - More.mp3
-    frank_sinatra_and_count_basie-more.mp3
-
-    Cornershop - Heavy Soup [Outro] [*] [*].mp3
-    cornershop-heavy_soup-outro.mp3
-
-    Soundtrack - American Pie 2\05 - Uncle Kracker - (Im Gonna) Split This Room In Half.mp3
-    soundtrack-american_pie_205-uncle_kracker-im_gonna-split_this_room_in_half.mp3
-
-    12. Only You (Bad Boys Remix)(Ft. The Notorious B.I.G.).mp3
-    12-only_you-bad_boys_remix-ft_the_notorious_b.i.g.mp3
-
-    Ultramagnetic MCs - Critical Beatdown.mp3.mp3.mp3
-    ultramagnetic_mcs-critical_beatdown.mp3
+In a nutshell, this means lowercasing everything and either getting
+rid of "funny" characters or replacing them with sane equivalents
+which allow the string to maintain some structure. See the test suite
+for a battery of examples.
 
 =head1 FUNCTIONS
 
 =head2 mfn
 
 Normalizes a string. Returns the normalized string. If no argument is
-given, mfn operates on C<$_>.
+given, C<$_> is used.
 
 =cut
 
 sub mfn {
     my $string = ( @_ ? $_[0] : $_ );
-    
-    $string =~ s/^[\{\[\(\-_]+//;         # drop leading {[(-_
-    $string =~ s/([a-z])([A-Z])/$1\_$2/g; # Insert '_' between caseSeparated words
-    $string =~ s/[\{\[\(\<>)\]\}]/-/g;    # change remaining {[(<>)]} to '-'
-    $string =~ s/^(\d+)\. /$1-/;          # handle things that look like track numbers
-    $string =~ s/\s+/_/g;                 # change whitespace to '_'
-    $string =~ s/^(\d+)([^\.\d])/$1-$2/;  # handle misc. initial sequence numbers
-    $string =~ s/\&/_and_/g;              # change '&' to "_and_"
-    $string =~ s/[^\w\-\.\+]//g;          # drop if not word, '-', '.', '+'
-    $string =~ s/_+-+/-/g;                # collapse _- sequences
-    $string =~ s/-+_+/-/g;                # collapse -_ sequences
-    $string =~ s/(\-|\_|\.)+/$1/g;        # collapse -_.
-    $string =~ s/(\-|\_|\.)$//;           # remove trailing -_. (rare)
-    $string =~ s/^(\-|\_|\.)//;           # remove leading -_. (rare-er)
-    $string =~ s/[_\-]+(\.[^\.]+)$/$1/;   # drop trailing -_ before extension
+    Encode::_utf8_on($string);
 
-    if ($string =~ /\.(\w+?)$/) {         # collapse
-	my $ext = $1;                     # repeat
-	$string =~ s/(\.$ext)+$/\.$ext/;  # extensions
+    # phase 1 - sanitize
+    $string =~ s/(\p{Lowercase})(\p{Uppercase})/$1_$2/g;  # inCap to in_Cap
+    $string =~ s/[\{\[\(\<>)\]\}~\|\/]/-/g;               # {[(<>)]}~|/ to '-'
+    $string =~ s/[\p{Zs}\t]+/_/g;                         # whitespace to '_'
+    $string =~ s/\&+/_and_/g;                             # '&' to "_and_"
+    $string =~ s/[^\p{Alphabetic}\p{Nd}\-\.\+_]//g;       # drop not-word chars
+
+    # phase 2 - condense
+    $string =~ s/_+-+/-/g;               # collapse _- sequences
+    $string =~ s/-+_+/-/g;               # collapse -_ sequences
+    $string =~ s/[\-\_]+\././g;          # collapse [-_]. sequences
+    $string =~ s/\.[\-\_]+/./g;          # collapse .[-_] sequences
+    $string =~ s/\-{2,}/-/g;             # collapse repeating -,
+    $string =~ s/\_{2,}/_/g;             #                    _,
+    $string =~ s/\.{2,}/./g;             #                and .
+    $string =~ s/^(\-|\_|\.)+//;         # remove leading -_.
+    $string =~ s/(\-|\_|\.)+$//;         # remove trailing -_. (rare)
+    if ($string =~ /\.(\w+?)$/) {        # collapse repeating extensions
+	my $ext = $1;
+	$string =~ s/(\.$ext)+$/\.$ext/;
     }
 
-    $string = lc($string);                # slam lowercase
-
-    return $string;
+    return lc($string);                   # slam lowercase
 }
 
-=head1 AUTHOR
+=head1 TODO
 
-Shawn Boyette, C<< <mdxi@cpan.org> >>
+=over
+
+=item *
+
+Add "classic" ASCII-oriented function for extra strictness
+
+=item *
+
+Add track/sequence number stuff to mfn(1p)
+
+=back
 
 =head1 BUGS
 
 =over
 
-=item internaCap separation only works on Latin characters with no diacriticals.
+=item *
+
+C<mfn()> forces Perl's C<_is_utf8> flag on, but does not attempt to
+verify that the data being passed to it is valid UTF-8.
 
 =back
-
 
 Please report any bugs or feature requests to
 C<bug-string-mfn@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
 be notified of progress on your bug as I make changes.
 
+=head1 AUTHOR
+
+Shawn Boyette, C<< <mdxi@cpan.org> >>
+
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2003-2005 Shawn Boyette, All Rights Reserved.
+Copyright 2003-2006 Shawn Boyette, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
